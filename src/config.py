677e66bae650236bd6f64b327f5105b25d0b2da9
maksim <knyazev.maksim2852@gmail.com>
2025-04-06 17:21:40 +0400

    добавлен конфиг

--- src/config.py
+++ src/config.py
@@ -16,6 +16,23 @@ class RedisConfig(BaseModel):
     port: int = Field(alias='REDIS_PORT', default=6379)
 
 
+class TelegramConfig(BaseModel):
+    token: str = Field(alias='TELEGRAM_TOKEN_BOT')
+
+
+class RabbitMQ(BaseModel):
+    user: str = Field(alias='RABBITMQ_DEFAULT_USER')
+    password: str = Field(alias='RABBITMQ_DEFAULT_PASS')
+
+
+class JWT(BaseModel):
+    secret_key: str = Field(alias='JWT_SECRET_KEY')
+    algorithm: str = Field(alias='ALGORITHM')
+
+
 class Config(BaseModel):
     postgres: PostgresConfig = Field(default_factory=lambda: PostgresConfig(**env))
     redis: RedisConfig = Field(default_factory=lambda: RedisConfig(**env))
+    telegram: TelegramConfig = Field(default_factory=lambda: TelegramConfig(**env))
+    rabbitmq: RabbitMQ = Field(default_factory=lambda: RabbitMQ(**env))
+    jwt: JWT = Field(default_factory=lambda: JWT(**env))
