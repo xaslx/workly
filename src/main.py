7517304383f8648930839ac7e806fbaa61cbc3fd
maksim <knyazev.maksim2852@gmail.com>
2025-04-06 19:37:44 +0400

    эндпоинт для 404 ошибки

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from dishka.integrations import fastapi as fastapi_integration
+from dishka.integrations.fastapi import inject, FromDishka as Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from src.presentation.controllers.setup_routers import setup_controllers
@@ -8,6 +9,7 @@ from src.config import Config
 from dishka import AsyncContainer, make_async_container
 from src.ioc import AppProvider
 from src.logger import logger
+from fastapi.templating import Jinja2Templates
 
 
 @asynccontextmanager
@@ -32,8 +34,11 @@ def create_app() -> FastAPI:
     setup_controllers(app=app)
     fastapi_integration.setup_dishka(container=container, app=app)
     
+    template: Jinja2Templates = Jinja2Templates(directory='src/presentation/static/html')
+
+
     @app.exception_handler(404)
     async def custom_404_handler(request: Request, __) -> HTMLResponse:
-        return RedirectResponse('/')
+        return template.TemplateResponse(request=request, name='404.html')
 
     return app
