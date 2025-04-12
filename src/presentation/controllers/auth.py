247d700a0399f9800500d87f606615bf5b154bff
maksim <knyazev.maksim2852@gmail.com>
2025-04-12 20:03:17 +0400

    изменены эндпоинты, добавлен новый для входа

--- src/presentation/controllers/auth.py
+++ src/presentation/controllers/auth.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, status, Request, HTTPException
+from fastapi import APIRouter, status, Request, HTTPException, Response
 from dishka.integrations.fastapi import inject, FromDishka as Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from src.application.use_cases.user.register import RegisterUserUseCase
-from src.presentation.schemas.code.code import SendCodeSchema, CheckCodeSchema
-from src.application.use_cases.code.code import SendCode, CheckCode
+from src.application.use_cases.auth.login import LoginUserUseCase
+from src.domain.user.entity import UserEntity
+from src.application.use_cases.auth.register import RegisterUserUseCase
+from src.presentation.schemas.code.code import LoginUserWithCode, SendCodeSchema, CheckCodeSchema
+from src.application.services.code import SendCode, CheckCode
 from src.application.dto.user.register import UserRegisterDTO
 from src.presentation.schemas.user.register import RegisterUserSchema
 
@@ -56,13 +58,15 @@ async def get_login_template(
 )
 @inject
 async def send_code(
-    telegram_id: SendCodeSchema,
+    send_code: SendCodeSchema,
     use_case: Depends[SendCode],
-):
-    print(f'TG ID: {telegram_id}')
-    res: bool = await use_case.execute(telegram_id=telegram_id.telegram_id)
+) -> bool:
+
+    res: bool = await use_case.execute(telegram_id=send_code.telegram_id, auth_type=send_code.auth_type)
+
     if res:
         return True
+    return False
 
 
 @router.post(
@@ -74,9 +78,10 @@ async def send_code(
 async def verify_code(
     code: CheckCodeSchema,
     use_case: Depends[CheckCode]
-):
+) -> bool:
+    
     res: bool = await use_case.execute(code=code.confirmation_code, telegram_id=code.telegram_id)
-    print(res)
+
     if not res:
         raise HTTPException(status_code=400, detail='Неверный код подтверждения')
     return res 
@@ -91,11 +96,28 @@ async def verify_code(
 async def register_user(
     new_user: RegisterUserSchema,
     use_case: Depends[RegisterUserUseCase]
-):
+) -> UserEntity:
+    
     user: UserRegisterDTO = UserRegisterDTO(
         telegram_id=new_user.telegram_id,
         name=new_user.name,
         username=new_user.username,
     )
-    res = await use_case.execute(new_user=user)
-    print(res)
+    user: UserEntity = await use_case.execute(new_user=user)
+    return user
+
+
+@router.post(
+    '/login',
+    description='Эндпоинт для входа',
+    status_code=status.HTTP_200_OK,
+)
+@inject
+async def login_user(
+    login_schema: LoginUserWithCode,
+    response: Response,
+    use_case: Depends[LoginUserUseCase],
+) -> None:
+    
+    token: str = await use_case.execute(telegram_id=login_schema.telegram_id, code=login_schema.confirmation_code)
+    response.set_cookie(key='user_access_token', value=token, max_age=10, httponly=True)
