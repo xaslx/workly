2ff5fd35f8c62066633a4039608e576feab1fcb6
maksim <knyazev.maksim2852@gmail.com>
2025-04-13 13:51:30 +0400

    добавлена зависимость пользователя

--- src/presentation/controllers/main.py
+++ src/presentation/controllers/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, status
 from dishka.integrations.fastapi import inject, FromDishka as Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from src.domain.user.entity import UserEntity
 
 
 router: APIRouter = APIRouter()
@@ -17,8 +18,11 @@ router: APIRouter = APIRouter()
 async def main_page(
     request: Request,
     template: Depends[Jinja2Templates],
+    user: Depends[UserEntity],
 ) -> HTMLResponse:
+
     return template.TemplateResponse(
         request=request,
         name='base.html',
+        context={'user': user}
     )
